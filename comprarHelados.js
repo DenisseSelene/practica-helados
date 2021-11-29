69970fcab2f7b3acdcacef436b4ff73d2ce86a1c
DenisseSelene <den.selene@gmail.com>
2021-11-28 22:50:07 -0300

    Agregamos personajes que compran helado

--- comprarHelados.js
+++ comprarHelados.js
@@ -1,11 +1,15 @@
 
 const compra = (nombre)=> {
     let dinero = prompt(`Cuánto dinero tenés en la billetera ${nombre}`);
-    if (dinero >= 0.50 && dinero < 1.20) return (`${nombre}, podés comprar un palito de agua!`);
-    if (dinero >= 1.20 && dinero < 1.50) return (`${nombre}, podés comprar un palito de crema!`);
-    if (dinero >= 1.50 && dinero <= 1.75) return (`${nombre}, podés comprar un cucurucho!`);
-    if (dinero >= 1.75 && dinero <= 2.50) return (`${nombre}, podés comprar un cucurucho doble!`);
-    if (dinero >= 2.50 ) return (`${nombre}, podés comprar un cuarto de helado!`);
+    if (dinero >= 0.50 && dinero < 1.20) return (`${nombre}, podés comprar un palito de agua! <br>`);
+    if (dinero >= 1.20 && dinero < 1.50) return (`${nombre}, podés comprar un palito de crema! <br>`);
+    if (dinero >= 1.50 && dinero <= 1.75) return (`${nombre}, podés comprar un cucurucho! <br>`);
+    if (dinero >= 1.75 && dinero <= 2.50) return (`${nombre}, podés comprar un cucurucho doble! <br>`);
+    if (dinero >= 2.50 ) return (`${nombre}, podés comprar un cuarto de helado! <br>`);
     else return (`${nombre}, no tenés plata suficiente para comprar ningun helado!`)
 }
+
+document.write(compra("Daryl"));
+document.write(compra("Michonne"));
+document.write(compra("Rick"));
 
